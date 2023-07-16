ccb5f240980a4b2417db05896b3b571ac406b293
Wesley0in <wesleyalvesskt15@gmail.com>
2023-07-16 09:12:13 -0300

    Versão 1.2

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,5 @@
-import React from 'react';
-
-function Header() {
-    return (
-        <div className="header">
-            <img src="url-logo-github" alt="Logo do GitHub"/>
-            <nav>
-                <a href="#">Features</a>
-                <a href="#">Business</a>
-                <a href="#">Explore</a>
-                <a href="#">Marketplace</a>
-                <a href="#">Pricing</a>
-            </nav>
-            <div>
-                <button>Sign in</button>
-                <button>Sign up</button>
-            </div>
-        </div>
-    )
+function Home(){
+    return <div> Aqui estamos meu jovemzinho, prontos para os 10 apps hahahahaha </div>
 }
 
-function Main() {
-    return (
-        <div className="main">
-            <h1>Built for developers</h1>
-            <p>GitHub is a development platform inspired by the way you work. From open source to business, you can host and review code, manage projects, and build software alongside millions of other developers.</p>
-            <button>Sign up for GitHub</button>
-        </div>
-    )
-}
-
-function Home() {
-    return (
-        <div>
-            <Header />
-            <Main />
-        </div>
-    )
-}
-
-export default Home;
+export default Home
