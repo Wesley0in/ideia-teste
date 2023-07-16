f64d1174231ed76d5d72ded5266b91fcf56640fa
Wesley0in <wesleyalvesskt15@gmail.com>
2023-07-16 09:07:14 -0300

    Versão 1.1

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Home(){
-    const [counter, setCounter] = useState(0); // iniciamos o contador com 0
+function Header() {
+    return (
+        <div className="header">
+            <img src="url-logo-github" alt="Logo do GitHub"/>
+            <nav>
+                <a href="#">Features</a>
+                <a href="#">Business</a>
+                <a href="#">Explore</a>
+                <a href="#">Marketplace</a>
+                <a href="#">Pricing</a>
+            </nav>
+            <div>
+                <button>Sign in</button>
+                <button>Sign up</button>
+            </div>
+        </div>
+    )
+}
 
-    const handleClick = () => {
-        setCounter(counter + 1); // toda vez que clicar no botão, incrementa o contador em 1
-    }
+function Main() {
+    return (
+        <div className="main">
+            <h1>Built for developers</h1>
+            <p>GitHub is a development platform inspired by the way you work. From open source to business, you can host and review code, manage projects, and build software alongside millions of other developers.</p>
+            <button>Sign up for GitHub</button>
+        </div>
+    )
+}
 
+function Home() {
     return (
         <div>
-            <p>Contador: {counter}</p> {/* exibe o valor do contador */}
-            <button onClick={handleClick}>Adicionar</button> {/* chama a função handleClick quando o botão é clicado */}
+            <Header />
+            <Main />
         </div>
-    );
+    )
 }
 
 export default Home;
